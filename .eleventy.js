e7ed8c50424f05d5f86b079eb5e25df48f20e5cc
sergiudan7 <dodeasergiud@gmail.com>
2025-04-16 11:51:41 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("script.js");
   eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("_headers");
-
+  eleventyConfig.addPassthroughCopy({ "static/_headers": "_headers" });
 
   return {
     dir: {
